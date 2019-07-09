64c56e646ecd3595465ef0942716729f8b04c219
Yafte M <redyafte@outlook.com>
2019-07-09 17:59:29 -0500

    added AsyncWebsocketConsumer

--- chat/routing.py
+++ chat/routing.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    # url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
-    path('/chat/<room_name>/', ChatConsumer)
+    path('ws/chat/<room_name>/', ChatConsumer)
 ]
